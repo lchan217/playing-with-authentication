022375cadfafc37cb593b10e2f745c1dcf6fe539
Lizzie <lchan217@gmail.com>
2019-11-14 15:46:55 -0700

    add twitter initializer and keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ opts = { scope: 'user:email' }
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], opts
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 end
