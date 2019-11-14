61c96037ac65e6442bdc174fd7668be8f7d5af33
Lizzie <lchan217@gmail.com>
2019-11-14 15:01:55 -0700

    add env to initializer google line

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ opts = { scope: 'user:email' }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], opts
-  provider :google_oauth2, "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
