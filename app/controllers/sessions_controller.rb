800558b7d6ed304e4e5c64ccc2f02876f5bccffe
Lizzie <lchan217@gmail.com>
2019-11-14 16:46:32 -0700

    remove extra controller line for google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,6 @@ class SessionsController < ApplicationController
   def google_create
     if access_token = request.env["omniauth.auth"]
       user = User.from_google_omniauth(access_token)
-      log_in(user)
       user.google_token = access_token.credentials.token
       refresh_token = access_token.credentials.refresh_token
       user.google_refresh_token = refresh_token if refresh_token.present?
