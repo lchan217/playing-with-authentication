d3b3ddca59e05d69fc8eac3201c67a06dd884e42
Lizzie <lchan217@gmail.com>
2019-11-14 13:49:45 -0700

    add google keys & initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ opts = { scope: 'user:email' }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], opts
+  provider :google_oauth2, "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'sessions#home'
 
   get '/auth/:provider/callback', to: 'sessions#github_create'
+  get '/auth/:provider/callback', to: 'sessions#google_create'
   get 'auth/failure', to: redirect('/')
 
   resources :podcasts
