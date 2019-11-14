3f24c490bcab7303b0fa7c42144f46118a289eba
Lizzie <lchan217@gmail.com>
2019-11-14 13:53:08 -0700

    add google columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_015919) do
+ActiveRecord::Schema.define(version: 2019_11_14_205239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_11_14_015919) do
     t.string "password_digest"
     t.string "provider"
     t.string "oauth_token"
+    t.string "google_token"
+    t.string "google_refresh_token"
   end
 
 end
