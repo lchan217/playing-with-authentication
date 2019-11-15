e28c947d39438c33266dfdc2e63c255d1803f0e7
Lizzie <lchan217@gmail.com>
2019-11-14 17:26:10 -0700

    change routes to more specific

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'sessions#home'
 
-  get '/auth/:provider/callback', to: 'sessions#github_create'
-  get '/auth/:provider/callback', to: 'sessions#google_create'
-  get '/auth/:provider/callback', to: 'sessions#twitter_create'
+  get '/auth/github/callback', to: 'sessions#github_create'
+  get '/auth/google_oauth2/callback', to: 'sessions#google_create'
+  get '/auth/twitter/callback', to: 'sessions#twitter_create'
   get 'auth/failure', to: redirect('/')
 
   resources :podcasts
